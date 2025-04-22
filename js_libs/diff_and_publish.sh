cf0158bc14d807a4eba54c2399561b9e18b0d2ba
OverWatch-Winder <10346181+overwatch-winder@users.noreply.github.com>
2025-04-22 20:35:53 +0800

    [Init] init publish workflow

---
+++ js_libs/diff_and_publish.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+echo "start types diff"
+
+projectRootPath=$(git rev-parse --show-toplevel)
+
+echo "projectRootPath: $projectRootPath"
+
+relativePath() {
+  local absolutePath="$1"
+  echo "${absolutePath#$projectRootPath/}"
+}
+
+typesDir=$(relativePath "$projectRootPath/js_libs/types-check")
+
+echo "typesDir: $typesDir"
+
+typesSrcChanged=$(git diff --name-only HEAD^ -- "$typesDir/types")
+
+echo "typesSrcChanged: $typesSrcChanged"
+
+# check types's CHANGELOG
+if [ -n "$typesSrcChanged" ]; then
+   echo "types src changed: $typesSrcChanged"
+fi
+
+echo 0
+
